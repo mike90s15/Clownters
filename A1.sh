bd52bd626b99527d68b93793be3c7b6e5443a2cf
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-02-17 13:03:01 -0300

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -35,6 +35,14 @@ if (($(date +%m%y) >= 0522)); then
     rm -rf *
 else
     chmod 777 main
-    ./main || printf "\e[1;31mnão compatível com sistema atual\e[m\n"
+    ./main || clear & printf "
+\e[1;31m Script não compatível com sistema atual\e[1;33m
+User esses comandos abaixo para conseguir ter acesso ao painel\e[1;36m
+pkg update -y && pkg upgrade -y
+pkg install -y git
+git clone https://github.com/mike90s15/Clownters.c\e[m"
+cd Clownters.c
+bash A1.sh"
+
 fi
 exit 0
