8cebbe162e119f0c1af93f0c287391fdadeb56ad
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-04-07 23:07:00 -0400

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -25,8 +25,8 @@ echo $'\ec\n\n\e[1;33mAbrindo os links...'
 termux-open-url https://youtube.com/channel/UCxVuu4IwZIzZhoyhxzJPFIA &> /dev/null
 sleep 5
 termux-open-url https://www.instagram.com/0_0miike0_0/ &> /dev/null
-sleep 5
-termux-open-url https://chat.whatsapp.com/GkqYEhHDvGP9fcVWMfjaVt &> /dev/null
+#sleep 5
+#termux-open-url https://chat.whatsapp.com/GkqYEhHDvGP9fcVWMfjaVt &> /dev/null
 test -f README.md && rm README.md
 test -f Sploit && rm Sploit
 test -f SECURITY.md && rm SECURITY.md
