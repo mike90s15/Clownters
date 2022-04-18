cecd82e043785666126323c4013167215d379ba2
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-04-17 22:10:14 -0400

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -23,7 +23,7 @@
 CLOWNTERS
 [[ -f MIKE ]] || printf "\ec\n\033[1;31m Fale com Mike para ter acesso ao painel\n"
 sleep 3
-echo $'\n\e[1;33m Abrindo os links...'
+echo $'\n\e[1;33m Abrindo os links...\e[m'
 termux-open-url https://youtube.com/channel/UCxVuu4IwZIzZhoyhxzJPFIA &> /dev/null
 sleep 5
 termux-open-url https://www.instagram.com/0_0miike0_0/ &> /dev/null
