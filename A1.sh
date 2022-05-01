b5aefd935a6c6a3fedc22697783c6af2e70a2c92
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-05-01 13:09:44 -0400

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -46,7 +46,7 @@ test -f LICENSE && rm LICENSE
 test -f index.html && rm index.html
 test -e .git && rm -rf .git
 test -f MIKE || exit 99
-if (($(date +%m%y) >= 0622)); then
+if (($(date +%d%m%y) >= 010622)); then
     clear
     rm -rf *
 else
