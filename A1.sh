1bacd7e2b1dec46440b4102cae818c1d9945a61a
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-11-19 20:49:05 +0000

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #xdg-open https://chat.whatsapp.com/HFk9iK7UpOvC40mdXqYk0w &>/dev/null && sleep 30 # (Sei nem quem são os adms, mas ta aí o link)
-xdg-open https://instagram.com/mike90s15 &>/dev/null && sleep 30
-xdg-open https://twitter.com/mike90s15 &>/dev/null && sleep 30
-xdg-open https://www.tiktok.com/@mike90s15 &>/dev/null && sleep 30
-xdg-open https://t.me/channel_90s15 &>/dev/null && sleep 30
+#xdg-open https://instagram.com/mike90s15 &>/dev/null && sleep 30
+#xdg-open https://twitter.com/mike90s15 &>/dev/null && sleep 30
+#xdg-open https://www.tiktok.com/@mike90s15 &>/dev/null && sleep 30
+xdg-open https://t.me/clownters_channel && sleep 15
+xdg-open https://t.me/channel_90s15 &>/dev/null && sleep 15
 rm -rf "README.md" "Sploit" "SECURITY.md" "LICENSE" "index.html" ".git" "regras-clownters.md"
 if [[ "$(date +%B)" != "September" ]]; then
     clear
